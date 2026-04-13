46a08485ebbca3bb3160bb62ce0900bb13761c2c
Atharva <your-email@example.com>
2026-04-13 14:43:12 +0530

    fixed root route properly

--- server.js
+++ server.js
@@ -21,13 +21,14 @@ const PORT   = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// ✅ ADD THIS LINE
+// 🔥 ADD THIS LINE BELOW
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/api/health', (req, res) => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use('/api/auth',         authRoutes);
 app.use('/api/projects',     projectsRoutes);
